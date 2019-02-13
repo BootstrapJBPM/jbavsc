c5d93529f1b2f30068bbe97a132e88562e088fd5
tsurdilo <tsurdilo@redhat.com>
2019-02-13 15:45:35 -0500

    fixing depends

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 "use strict";
 
-import { window, commands, ExtensionContext, Uri } from "vscode";
+import { commands, ExtensionContext, Uri } from "vscode";
 import { startGen } from "./lib/generate/startGen";
 import { startDebugger } from "./lib/debug/debugMonitor";
 import { startPreview } from "./lib/preview/startPreview";
